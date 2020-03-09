9d3b248dd88c91776560810e112f38230d09b58b
reiden <35837918+pepepper@users.noreply.github.com>
2020-03-09 15:45:34 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -30,9 +30,9 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 # Hack some props to allow stock ROM flashing
-BOARD_RECOVERY_IMAGE_PREPARE := \
-    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.product.device=.*/ro.product.device=ASUS_X01BD/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
+#BOARD_RECOVERY_IMAGE_PREPARE := \
+#    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+#    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+#    sed -i 's/ro.product.device=.*/ro.product.device=ASUS_X01BD/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
 
 endif
